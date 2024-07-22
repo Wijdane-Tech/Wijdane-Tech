3e7af0da5fcb44532b740b959a54e48e41d60769
Wijdane-Tech <Wijdane.Zeriouh@hotmail.com>
2024-07-22 22:42:27 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,6 @@
+- 👋 Hi, I’m @Wijdane-Tech
+- 👀 I’m interested in AI tools
+- 🌱 I’m currently learning the basics of computer science
+- 💞️ I’m looking to collaborate on a project for beginners
+- 📫 How to reach me : telegram @Wijjdane
+- ⚡ Fun fact: I Only code with excel VBA to automate my tasks at work
